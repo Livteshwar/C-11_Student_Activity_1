1237be9d06e66f5a3028be23827d4610bc864ca6
Livteshwar <84121496+Livteshwar@users.noreply.github.com>
2021-06-12 19:26:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,6 +9,9 @@ function setup() {
   s4 = createSprite(350, 150, 30, 30);
   
   sprites = [s1,s2,s3,s4]; 
+  for(var i=0;i<sprites.length; i= i+1) { 
+    console.log(sprites[i].position.x);
+  }
   
  
 }
